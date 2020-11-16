2f23816befca2fa1581102f7489dc092ede9202a
Hitesh Das <hitesh.das@olx.com>
2020-11-16 21:55:39 +0530

    Allow multiple files for proto file

--- app/src/main/java/com/demo/jetpackdatastore/data/proto/model/Ad.kt
+++ app/src/main/java/com/demo/jetpackdatastore/data/proto/model/Ad.kt
@@ -1,9 +1,7 @@
 package com.demo.jetpackdatastore.data.proto.model
 
-import java.io.Serializable
-
 data class Ad(
     val title: String,
     val type: AdType,
     val category: AdCategory
-) : Serializable
+)
